755aaf91f20c7a46cfd6aa7a1aa2af8ad1f03647
Fernando FR <ferlicio@gmail.com>
2022-07-28 17:45:49 -0300

    Update UserService.java

--- Servicos/src/main/java/com/soulcode/Servicos/Services/UserService.java
+++ Servicos/src/main/java/com/soulcode/Servicos/Services/UserService.java
@@ -6,6 +6,7 @@ import com.soulcode.Servicos.Util.TokenUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import javax.management.RuntimeErrorException;
 import java.util.List;
 import java.util.Optional;
 
